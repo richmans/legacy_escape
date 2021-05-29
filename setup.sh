5ecdb14256017f8a11bfd6e29b5d9ede30159060
richmans <richmans@users.noreply.github.com>
2021-05-29 13:07:50 +0100

    added setup

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if lsmod | grep spi_ ; then
+  echo "Spi module detected"
+else 
+  echo "SPI module not detected. You will need to reboot after this!"
+  sudo bash -c 'echo "dtparam=spi=on" >> /boot/config.txt'
+fi
+
+sudo apt install -y python3-spidev
